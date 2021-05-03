9ef9a4646055f6a6c575c517367dcb8845ffa2ca
w_dop <w_doppie@hotmail.com>
2021-05-03 14:46:12 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,40 @@
+
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+use web_sys::*;
+use web_sys::WebGlRenderingContext as GL;
+
+mod gl_setup;
+
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str); 
+}
+
+#[wasm_bindgen]
+pub struct RustClient {
+    gl: WebGlRenderingContext,
+}
+
+#[wasm_bindgen]
+impl RustClient {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Self {
+        console_error_panic_hook::set_once();
+        let gl = gl_setup::initialize_webgl_context().unwrap();
+
+        Self {
+            gl: gl,
+        }
+    }
+
+    pub fn update( &mut self, _time: f32, _height: f32, _width: f32) -> Result<(), JsValue> {
+
+        Ok(())
+    }
+
+    pub fn render(&self) {
+        self.gl.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT);
+    }
+}
